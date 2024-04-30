20127abdff3296a54042a96800d50c34fdff8b6e
art-mtsh <108072766+art-mtsh@users.noreply.github.com>
2024-04-30 18:21:21 +0300

    123

--- backend/migrate_all.py
+++ backend/migrate_all.py
@@ -5,19 +5,19 @@ from django.core.management import call_command
 
 
 def main():
-    # # встановити перемінну оточення
-    # os.environ['ENVIRONMENT'] = 'development'
-    # # додати цей файл в PATH
-    # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    # # встановити перемінну оточення DJANGO_SETTINGS_MODULE
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-    # # ініціалізація DJANGO
-    # django.setup()
-    # # виконання міграцій та запуску сервера
-    # call_command("makemigrations")
-    # call_command("migrate")
-    #
-    # print(f"Migrated with {os.environ['ENVIRONMENT'].upper()} environment!\n")
+    # встановити перемінну оточення
+    os.environ['ENVIRONMENT'] = 'development'
+    # додати цей файл в PATH
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    # встановити перемінну оточення DJANGO_SETTINGS_MODULE
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+    # ініціалізація DJANGO
+    django.setup()
+    # виконання міграцій та запуску сервера
+    call_command("makemigrations")
+    call_command("migrate")
+
+    print(f"Migrated with {os.environ['ENVIRONMENT'].upper()} environment!\n")
 
     # встановити перемінну оточення
     os.environ['ENVIRONMENT'] = 'production'
