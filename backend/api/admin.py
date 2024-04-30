267bf24a5d49520dfe7da0e44c4dc6313c9097a5
art-mtsh <108072766+art-mtsh@users.noreply.github.com>
2024-04-30 16:02:37 +0300

    123

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(RoomType)
+admin.site.register(ItemCategory)
+admin.site.register(Manufacturer)
+admin.site.register(ItemCollection)
+admin.site.register(Items)
+admin.site.register(ItemColour)
+admin.site.register(ItemMaterial)
+admin.site.register(ItemPhoto)
+admin.site.register(ItemReview)
