96a819f9d381db65bcdf5a096fb8b57ba636d52a
art-mtsh <108072766+art-mtsh@users.noreply.github.com>
2024-05-06 15:26:13 +0300

    Changed button_view to block=True

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,7 +14,7 @@ from sematext import log_engine
 def button_view(request):
     return JsonResponse({'message': 'Hello World'})
 
-def test():
+def test123():
     pass
 
 class RoomTypeView(generics.ListCreateAPIView):
