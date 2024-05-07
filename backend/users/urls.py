94c48d0488b3bf8fed7375cdd537a7231f1064ac
art-mtsh <108072766+art-mtsh@users.noreply.github.com>
2024-05-07 13:57:27 +0300

    Separated api sections, added users auth api

---
+++ backend/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+urlpatterns = [
+    path('create-user/', CreateUserView.as_view(), name='register'),
+    path('get-token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('refresh-token/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('auth/', include('rest_framework.urls')),
+]
